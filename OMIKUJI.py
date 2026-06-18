bc3a9e1c0c24e8dac2c6c9c9f72f768aea336f47
takekuma1165 <takekuma6643@gmail.com>
2026-06-18 20:43:03 +0900

    コメント修正

--- OMIKUJI.py
+++ OMIKUJI.py
@@ -2,7 +2,7 @@ import random
 
 def omikuji():
     results = ["大吉", "中吉", "小吉", "吉", "末吉", "凶"]
-    weights = [2, 1, 1, 1, 1, 0.5]  # 大吉の確率を 4 倍、凶の確率を 1/3 に調整
+    weights = [2, 1, 1, 1, 1, 0.5]  # 大吉の確率を 5 倍、凶の確率を 1/3 に調整
     result = random.choices(results, weights=weights, k=1)[0]
     print("あなたの運勢は・・・")
     print(result)
